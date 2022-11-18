65c3f788a4108577aad497e9758213cad452a343
berserkr1 <g.coirier@epmistes.net>
2022-11-18 12:16:07 +0100

    quest Symfony 09 Fixtures with joins

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function getDependencies()
+    {
+        return [
+            CategoryFixtures::class,
+        ];
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $numberOfCategories = count(CategoryFixtures::CATEGORIES);
+        for ($i = 1; $i <= 25; $i++) {
+            $program = new Program();
+            $program->setTitle('Title ' . $i);
+            $program->setSynopsis('Synopsis ' . $i);
+            $program->setPoster('Link to poster ' . $i);
+            $program->setCategory($this->getReference('category_' . CategoryFixtures::CATEGORIES[rand(0, $numberOfCategories - 1)]));
+            $manager->persist($program);
+        }
+        $manager->flush();
+    }
+}
