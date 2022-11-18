0280d9b16816b8c606d8ec1ab54d181042e93483
berserkr1 <g.coirier@epmistes.net>
2022-11-18 16:56:07 +0100

    quest Symfony 10 Retrieve data with Doctrine

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,6 +2,7 @@
 // src/Controller/ProgramController.php
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,16 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
     #[Route('', name: 'index')]
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
-        return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
-        ]);
+        $programs = $programRepository->findAll();
+        return $this->render('program/index.html.twig', ['programs' => $programs]);
     }
 
     #[Route('{id<\d+>}', methods: ['GET'], name: 'show')]
-    public function show(int $id): Response
+    public function show(int $id, ProgramRepository $programRepository): Response
     {
-        return $this->render('program/show.html.twig', ['id' => $id]);
+        $program = $programRepository->findOneById($id);
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'Pas de série avec l\'id : ' . $id
+            );
+        }
+        return $this->render('program/show.html.twig', ['program' => $program]);
     }
 }
