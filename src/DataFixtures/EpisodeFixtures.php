2a6a262293ed028ef5c8df2f3693f224b8669ada
berserkr1 <g.coirier@epmistes.net>
2022-11-20 09:14:55 +0100

    quest Symfony 12 Create data pools with FakerPHP

--- src/DataFixtures/EpisodeFixtures.php
+++ src/DataFixtures/EpisodeFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Episode;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -18,19 +19,20 @@ class EpisodeFixtures extends Fixture implements DependentFixtureInterface
 
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create();
         for ($i = 1; $i <= 25; $i++) {
             $programOfReference = $this->getReference('program_Title_' . $i);
             $numberOfSeasons = count($programOfReference->getSeasons());
             for ($j = 1; $j <= $numberOfSeasons; $j++) {
                 $seasonOfReference = $this->getReference('program_' . $programOfReference->getTitle() . '_S' . $j);
-                $numberOfEpisodes = rand(2, 6);
+                $numberOfEpisodes = rand(10, 20);
                 $k = 1;
                 while ($k <= $numberOfEpisodes) {
                     $episode = new Episode();
                     $episode->setSeason($seasonOfReference);
-                    $episode->setTitle('Title ' . $k);
+                    $episode->setTitle($faker->sentence($faker->numberBetween(1, 8)));
                     $episode->setNumber($k);
-                    $episode->setSynopsis('Synopsis episode ' . $k);
+                    $episode->setSynopsis($faker->paragraphs(3, true));
                     $manager->persist($episode);
                     $k++;
                 }
--- src/DataFixtures/SeasonFixtures.php
+++ src/DataFixtures/SeasonFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Season;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class SeasonFixtures extends Fixture implements DependentFixtureInterface
 {
@@ -18,17 +19,18 @@ class SeasonFixtures extends Fixture implements DependentFixtureInterface
 
     public function load(ObjectManager $manager): void
     {
+        $faker = Factory::create();
         for ($i = 1; $i <= 25; $i++) {
             $programOfReference = $this->getReference('program_Title_' . $i);
-            $firstSeasonYear = rand(1990, 2021);
-            $numberOfSeasons = rand(1, 7);
+            $firstSeasonYear = $faker->numberBetween(1990, 2021);
+            $numberOfSeasons = $faker->numberBetween(1, 10);
             $j = 1;
             while (($j <= $numberOfSeasons) && ($firstSeasonYear + $j <= 2022)) {
                 $season = new Season();
                 $season->setProgram($programOfReference);
                 $season->setNumber($j);
                 $season->setYear($firstSeasonYear + $j - 1);
-                $season->setDescription('Description season ' . $j);
+                $season->setDescription($faker->paragraphs(3, true));
                 $manager->persist($season);
                 $this->addReference('program_' . $programOfReference->getTitle() . '_S' . $j, $season);
                 $j++;
